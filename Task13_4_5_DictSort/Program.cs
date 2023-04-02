9dab86b89855569ae92e5bb744e534da6abdcaff
visot <visotskiy.alexey@gmail.com>
2023-04-02 13:58:18 +0700

    Проекты для заданий 13.4.5

---
+++ Task13_4_5_DictSort/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task13_4_5_DictSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
