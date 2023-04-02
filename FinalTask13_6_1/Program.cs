9dc43b2add51bf7ce235a0e1a9bac4d24b830a5f
visot <visotskiy.alexey@gmail.com>
2023-04-02 15:57:07 +0700

    Итоговое задание 1

---
+++ FinalTask13_6_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+namespace FinalTask13_6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllText("Text1.txt");
+            char[] delimitersChars = new char[] { ' ', '\r', '\n' };
+            string[] words = text.Split(delimitersChars, StringSplitOptions.RemoveEmptyEntries);
+            
+            //List
+            List<string> list = new List<string>();
+
+            var duration1 = Stopwatch.StartNew();
+
+            for (int i = 0; i < words.Length; i++)
+                list.Add(words[i]);
+
+            Console.WriteLine("List: "+duration1.Elapsed.TotalMilliseconds);
+
+            //LinkedList
+            LinkedList<string> linkedList = new LinkedList<string>();
+
+            var duration2 = Stopwatch.StartNew();
+
+            for (int i = 0; i < words.Length; i++)
+                linkedList.AddFirst(words[i]);
+           
+            Console.WriteLine("LinkedList: "+ duration2.Elapsed.TotalMilliseconds);
+       }
+    }
+}
