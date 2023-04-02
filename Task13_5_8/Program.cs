c7d757c2f8cb3ab0f969f978a0f731ea259f9fc5
visot <visotskiy.alexey@gmail.com>
2023-04-02 15:14:38 +0700

    Задание 13.5.8

---
+++ Task13_5_8/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Concurrent;
+
+namespace Task13_5_8
+{
+    class Program
+    {
+        // объявим потокобезопасную очередь (полностью идентична обычной очереди, но
+        // позволяет безопасный доступ
+        // из разных потоков)
+        public static ConcurrentQueue<string> words = new ConcurrentQueue<string>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово и нажмите Enter, чтобы добавить его в очередь.");
+            Console.WriteLine();
+
+            //  запустим обработку очереди в отдельном потоке
+            StartQueueProcessing();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "peek")
+                {
+                    if (words.TryPeek(out var peeked))
+                        Console.WriteLine(peeked);
+                }
+                else
+                {
+                    // если не введена - ставим элемент в очередь, как и обычно
+                    words.Enqueue(input);
+                }
+            }
+        }
+
+        // метод, который обрабатывает и разбирает нашу очередь в отдельном потоке
+        // ( для выполнения задания изменять его не нужно )
+        static void StartQueueProcessing()
+        {
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+
+                while (true)
+                {
+                    Thread.Sleep(500);
+                    if (words.TryDequeue(out var element))
+                        Console.WriteLine("======>  " + element + " прошел очередь");
+                }
+
+            }).Start();
+        }
+    }
+}
